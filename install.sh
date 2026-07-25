8abd1946cebec77a9ec53873adee1feabadebeff
Thiago Souza <thiagocvs2003@gmail.com>
2026-07-25 17:59:18 +0100

    add syncthing installation

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 SYNC_DIR_NAME='synced-directories'
-  
+
+# ID único da pasta (precisa ser IGUAL em todos os dispositivos)
+COLD_BACKUP_ID="0hdd-storage-cold-backup"
+DRAWINGS_ID="krita-drawings"
+EMACS_ID="emac-main"
+OBSIDIAN_ID="nsya9-d4te9"
 
 # 1. Detectar o Sistema Operacional
 OS="$(uname -s)"
@@ -9,10 +14,47 @@ case "${OS}" in
     # Verifica se é Android (Termux) ou Linux Desktop
     if [ -d "/data/data/com.termux" ]; then
       echo "Detectado: Android (Termux)"
+      THIS_OS_TYPE="TERMUX"
       CONFIG_DIR="$HOME/.local/state/syncthing"
       SYNC_PATH="$HOME/Storage/$SYNC_DIR_NAME"
+      PKG_MANAGER="pkg install -y"
     else
       echo "Detectado: Linux"
+      if [[ -f /etc/os-release ]] && command -v systemctl &>/dev/null; then
+        source /etc/os-release
+        # A variável $ID contém o nome curto da distro (ex: ubuntu, debian, fedora)
+        case "$ID" in
+          ubuntu|debian)
+            echo "Está a usar uma base Debian ($ID)"
+            PKG_MANAGER="sudo apt install -y"
+            THIS_OS_TYPE="debian"
+            echo "$PKG_MANAGER"
+            ;;
+          fedora|rhel|centos)
+            echo "Estás a usar uma base Red Hat ($ID)"
+            PKG_MANAGER="sudo dnf install -y"  
+            THIS_OS_TYPE="fedora"
+            echo "$PKG_MANAGER"
+            ;;
+          alpine)
+            echo "Estás a usar Alpine Linux"
+            PKG_MANAGER="apk add"
+            THIS_OS_TYPE="alpine"
+            echo "$PKG_MANAGER"
+            ;;
+          arch)
+            echo "Estás a usar Arch Linux"
+            PKG_MANAGER="sudo pacman -S --noconfirm"
+            THIS_OS_TYPE="arch"
+            echo "$PKG_MANAGER"
+            ;;
+          *)
+            echo "Distribuição Linux não identificada: $ID"
+            echo  "Vou compilar o nvim mesmo assim, me diz se funcionou :D"
+            THIS_OS_TYPE="other_linux"
+            ;;
+        esac
+      fi
       if [ -d "$HOME/.local/state/syncthing" ]; then
         CONFIG_DIR="$HOME/.local/state/syncthing"
       elif [ -d "$HOME/.config/syncthing" ]; then
@@ -28,6 +70,7 @@ case "${OS}" in
     echo "Detectado: macOS"
     CONFIG_DIR="$HOME/Library/Application Support/Syncthing"
     SYNC_PATH="$HOME/$SYNC_DIR_NAME"
+    PKG_MANAGER="brew install"
     ;;
   CYGWIN*|MINGW32*|MSYS*|MINGW*)
     echo "Detectado: Windows"
@@ -40,9 +83,27 @@ case "${OS}" in
     ;;
 esac
 
+if ! command -v syncthing; then
+  "$PKG_MANAGER" syncthing
+fi
+
+case "$THIS_OS_TYPE" in
+  ubuntu|fedora|arch)
+    echo "Ativando serviço via Systemd..."
+    systemctl --user enable --now syncthing.service
+    ;;
+  TERMUX)
+    echo "Ativando serviço via Termux-Services..."
+    if ! command -v sv-enable &>/dev/null; then
+      pkg install termux-services -y
+    fi
+    sv-enable syncthing
+    sv up syncthing 
+    ;;
+esac
+
 # 2. Criar o diretório de sincronização se não existir
 mkdir -p "$SYNC_PATH"
-
 
 
 # 3. Validar se o Syncthing já gerou o config.xml inicial
@@ -50,13 +111,6 @@ if [ ! -f "$CONFIG_DIR/config.xml" ]; then
   echo "Erro: config.xml não encontrado. Inicie o Syncthing uma vez para gerar o arquivo base."
   exit 1
 fi
-
-# ID único da pasta (precisa ser IGUAL em todos os dispositivos)
-#
-COLD_BACKUP_ID="0hdd-storage-cold-backup"
-DRAWINGS_ID="krita-drawings"
-EMACS_ID="emac-main"
-OBSIDIAN_ID="nsya9-d4te9"
 
 echo "Configurando a pasta no diretório: $CONFIG_DIR"
 
